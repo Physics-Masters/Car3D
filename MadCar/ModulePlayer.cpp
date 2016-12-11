27428292f4629f6eae4392c12a9f5f2340551fe6
Pere Rifà Planellas <pere.rifaplanellas@gmail.com>
2016-12-11 09:35:05 +0100

    Added repositiory and project with basic car 3D

---
+++ MadCar/ModulePlayer.cpp
@@ -0,0 +1,189 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModulePlayer.h"
+#include "Primitive.h"
+#include "PhysVehicle3D.h"
+#include "PhysBody3D.h"
+
+ModulePlayer::ModulePlayer(Application* app, bool start_enabled) : Module(app, start_enabled), vehicle(NULL)
+{
+	turn = acceleration = brake = 0.0f;
+}
+
+ModulePlayer::~ModulePlayer()
+{}
+
+// Load assets
+bool ModulePlayer::Start()
+{
+	LOG("Loading player");
+
+	VehicleInfo car;
+	
+	// Car properties ----------------------------------------
+	car.num_chassis = 7;
+	car.chassis_size = new vec3[car.num_chassis];
+	car.chassis_size[0].Set(0.75f, 0.5f, 4.25f);
+	car.chassis_size[1].Set(0.3f, 1, 0.1f);
+	car.chassis_size[2].Set(1, 0.8f, 1.5f);
+	car.chassis_size[3].Set(1.8f, 0.25f, 0.25f);
+	car.chassis_size[4].Set(1.5f, 0.15f, 0.15f);
+	car.chassis_size[5].Set(2, 0.1f, 0.35f);
+	car.chassis_size[6].Set(1.5f, 0.35f, 1.85f);
+
+	
+	car.chassis_offset = new vec3[car.num_chassis];
+	car.chassis_offset[0].Set(0, 0.5f, 0);
+	car.chassis_offset[1].Set(0, 1.0f, -2.25f);
+	car.chassis_offset[2].Set(0, 0.75f, -0.9f);
+	car.chassis_offset[3].Set(0, 0.65f, -2.1f);
+	car.chassis_offset[4].Set(0, 0.5f, 2);
+	car.chassis_offset[5].Set(0, 1.5f, -2.25f);
+	car.chassis_offset[6].Set(0, 0.6f, -0.6f);
+	
+	
+
+	car.mass = 500.0f;
+	car.suspensionStiffness = 15.88f;
+	car.suspensionCompression = 0.83f;
+	car.suspensionDamping = 0.88f;
+	car.maxSuspensionTravelCm = 1000.0f;
+	car.frictionSlip = 50.5;
+	car.maxSuspensionForce = 6000.0f;
+	
+
+	// Wheel properties ---------------------------------------
+	float connection_height = 0.5f;
+	float wheel_radius = 0.5f;
+	float wheel_width = 0.5f;
+	float suspensionRestLength = 0.5f;
+
+	// Don't change anything below this line ------------------
+
+	float half_width = 3*0.5f;
+	float half_length = 5*0.5f;
+	
+	vec3 direction(0,-1,0);
+	vec3 axis(-1,0,0);
+	
+	car.num_wheels = 4;
+	car.wheels = new Wheel[4];
+
+	// FRONT-LEFT ------------------------
+	car.wheels[0].connection.Set(half_width - 0.3f * wheel_width, connection_height, half_length - wheel_radius);
+	car.wheels[0].direction = direction;
+	car.wheels[0].axis = axis;
+	car.wheels[0].suspensionRestLength = suspensionRestLength;
+	car.wheels[0].radius = wheel_radius;
+	car.wheels[0].width = wheel_width;
+	car.wheels[0].front = true;
+	car.wheels[0].drive = true;
+	car.wheels[0].brake = false;
+	car.wheels[0].steering = true;
+
+	// FRONT-RIGHT ------------------------
+	car.wheels[1].connection.Set(-half_width + 0.3f * wheel_width, connection_height, half_length - wheel_radius);
+	car.wheels[1].direction = direction;
+	car.wheels[1].axis = axis;
+	car.wheels[1].suspensionRestLength = suspensionRestLength;
+	car.wheels[1].radius = wheel_radius;
+	car.wheels[1].width = wheel_width;
+	car.wheels[1].front = true;
+	car.wheels[1].drive = true;
+	car.wheels[1].brake = false;
+	car.wheels[1].steering = true;
+
+	// REAR-LEFT ------------------------
+	car.wheels[2].connection.Set(half_width - 0.3f * wheel_width, connection_height, -half_length + wheel_radius);
+	car.wheels[2].direction = direction;
+	car.wheels[2].axis = axis;
+	car.wheels[2].suspensionRestLength = suspensionRestLength;
+	car.wheels[2].radius = wheel_radius;
+	car.wheels[2].width = wheel_width;
+	car.wheels[2].front = false;
+	car.wheels[2].drive = false;
+	car.wheels[2].brake = true;
+	car.wheels[2].steering = false;
+
+	// REAR-RIGHT ------------------------
+	car.wheels[3].connection.Set(-half_width + 0.3f * wheel_width, connection_height, -half_length + wheel_radius);
+	car.wheels[3].direction = direction;
+	car.wheels[3].axis = axis;
+	car.wheels[3].suspensionRestLength = suspensionRestLength;
+	car.wheels[3].radius = wheel_radius;
+	car.wheels[3].width = wheel_width;
+	car.wheels[3].front = false;
+	car.wheels[3].drive = false;
+	car.wheels[3].brake = true;
+	car.wheels[3].steering = false;
+
+	vehicle = App->physics->AddVehicle(car);
+	vehicle->SetPos(0, 12, 10);
+	
+	return true;
+}
+
+// Unload assets
+bool ModulePlayer::CleanUp()
+{
+	LOG("Unloading player");
+
+	return true;
+}
+
+ btVector3 ModulePlayer::GetVehiclePosition()
+{
+	 return vehicle->vehicle->getRigidBody()->getWorldTransform().getOrigin();
+}
+
+// Update: draw background
+update_status ModulePlayer::Update(float dt)
+{
+	turn = acceleration = brake = 0.0f;
+	int t;
+	if (App->input->GetKey(SDL_SCANCODE_T) == KEY_REPEAT)
+	{ 
+		t = 3;
+	}
+	else t = 1;
+	if(App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+	{
+		acceleration = t*MAX_ACCELERATION;
+	}
+
+	if(App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+	{
+		if(turn < TURN_DEGREES)
+			turn +=  TURN_DEGREES;
+	}
+
+	if(App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+	{
+		if(turn > -TURN_DEGREES)
+			turn -= TURN_DEGREES;
+	}
+
+	if(App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+	{
+		brake = BRAKE_POWER;
+	}
+	if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN)
+	vehicle->Push(0, 10*vehicle->info.mass, 0);
+
+	
+	vehicle->ApplyEngineForce(acceleration);
+	vehicle->Turn(turn);
+	vehicle->Brake(brake);
+	
+	
+	vehicle->Render();
+
+	char title[80];
+	sprintf_s(title, "%.1f Km/h", vehicle->GetKmh());
+	App->window->SetTitle(title);
+	
+	return UPDATE_CONTINUE;
+}
+
+
+
